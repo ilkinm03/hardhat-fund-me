2073e20d5dec34150f58da0128aadb0a30370a25
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2024-04-04 21:52:55 +0400

    chore: install extra hardhat deps

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
