1f3184f6d6a4725771b30b7c6cfc2f602a6bc7a3
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2024-04-06 12:15:21 +0400

    chore: set up configs for hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 require("hardhat-deploy");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
 
+const { ETHERSCAN_API_KEY, SEPOLIA_RPC_URL, COINMARKETCAP_API_KEY, PRIVATE_KEY } = process.env;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+    solidity: "0.8.24",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY
+    },
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        }
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            1: 0
+        }
+    }
 };
