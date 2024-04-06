25729dcf778bee3a1dd645877b4e6480aea7b936
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2024-04-06 19:10:52 +0400

    feat(fundMe): create cheaperWithdraw function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -55,4 +55,15 @@ contract FundMe {
         (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "call failed");
     }
+
+    function cheaperWithdraw() public payable onlyOwner {
+        address[] memory funders = s_funders;
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "call failed");
+    }
 }
